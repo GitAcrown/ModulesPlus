2f48e0bc6c48aba1cf4d1659b76bd02562422be5
GitAcrown <Acrown@outlook.fr>
2018-12-29 20:42:29 +0100

    Correctifs et améliorations

--- karma/karma.py
+++ karma/karma.py
@@ -482,11 +482,10 @@ class Karma:
     @commands.command(pass_context=True)
     async def em(self, ctx):
         """Test Embed"""
-        em = discord.Embed(title="Test")
         provide = {
-            'name' : "Jean-provider"
+            'name': "Jean-provider"
         }
-        em.provider = provide
+        em = discord.Embed(title="Test", provider = provide)
         await self.bot.say(embed=em)
 
     @commands.group(name="prisonset", aliases=["pset"], pass_context=True)
