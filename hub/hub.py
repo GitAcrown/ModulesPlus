4b5d75b0699cae6971e6e768b61deae3f76733f4
GitAcrown <Acrown@outlook.fr>
2019-04-22 18:52:58 +0200

    kk

--- hub/hub.py
+++ hub/hub.py
@@ -25,7 +25,7 @@ class Hub:
         self.cache = {}
 
     def can_interact(self, user):
-        today = datetime.strftime("%d/%m/%Y")
+        today = datetime.strptime(datetime.now(), "%d/%m/%Y")
         if user.id not in self.cache:
             self.cache[user.id] = ""
         if self.cache[user.id] != today:
